9dd9bb55056b4aa4243e9eef7c60dfbf4f040d4f
voiteshonok <shnobdog1@gmail.com>
2021-04-15 19:29:45 +0300

    Apply black formatting

--- ml_project/src/visualization/eda.py
+++ ml_project/src/visualization/eda.py
@@ -7,7 +7,7 @@ from src.data.make_dataset import read_data
 from pathlib import Path
 
 
-@hydra.main(config_path='../../conf', config_name='config')
+@hydra.main(config_path="../../conf", config_name="config")
 def main(cfg: DictConfig) -> None:
     data = read_data(get_path_from_root(cfg.eda.input_data_path))
     report = hydra.utils.instantiate(cfg.eda.eda_class, data)
@@ -16,5 +16,5 @@ def main(cfg: DictConfig) -> None:
     report.to_file(os.path.join(report_dir, cfg.eda.report_file_name))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
