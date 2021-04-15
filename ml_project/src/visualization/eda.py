12b14bcc0a719e2b0f6b92b6c315fdab0b75475c
voiteshonok <shnobdog1@gmail.com>
2021-04-15 17:57:17 +0300

    Add function to read data

--- ml_project/src/visualization/eda.py
+++ ml_project/src/visualization/eda.py
@@ -1,16 +1,16 @@
 import hydra
-import pandas as pd
 import os
 
 from omegaconf import DictConfig
 from src.utils.utils import get_path_from_root
+from src.data.make_dataset import read_data
 from pathlib import Path
 
 
 @hydra.main(config_path='../../conf', config_name='config')
 def main(cfg: DictConfig) -> None:
-    df = pd.read_csv(get_path_from_root(cfg.eda.input_data_path))
-    report = hydra.utils.instantiate(cfg.eda.eda_class, df)
+    data = read_data(get_path_from_root(cfg.eda.input_data_path))
+    report = hydra.utils.instantiate(cfg.eda.eda_class, data)
     report_dir = get_path_from_root(cfg.eda.report_dir)
     Path(report_dir).mkdir(parents=True, exist_ok=True)
     report.to_file(os.path.join(report_dir, cfg.eda.report_file_name))
