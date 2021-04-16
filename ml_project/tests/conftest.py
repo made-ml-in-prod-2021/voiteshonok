c744d96274ea58d43b3c4b9ef916dc22f4420282
voiteshonok <shnobdog1@gmail.com>
2021-04-16 12:38:09 +0300

    Add test for formatting with black

--- ml_project/tests/conftest.py
+++ ml_project/tests/conftest.py
@@ -3,6 +3,16 @@ import os
 
 
 @pytest.fixture()
+def src_path():
+    return "src"
+
+
+@pytest.fixture()
+def tests_path():
+    return "tests"
+
+
+@pytest.fixture()
 def dataset_path():
     curdir = os.path.dirname(__file__)
     return os.path.join(curdir, "data_sample.zip")
