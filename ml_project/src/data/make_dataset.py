a4175b57c2f792b3f40764a5805249dc5017b9fa
voiteshonok <shnobdog1@gmail.com>
2021-04-16 11:36:32 +0300

    Add logging

--- ml_project/src/data/make_dataset.py
+++ ml_project/src/data/make_dataset.py
@@ -1,6 +1,12 @@
 import pandas as pd
+import logging
+
+
+logger = logging.getLogger("mlProject.data.make_dataset")
 
 
 def read_data(path: str) -> pd.DataFrame:
+    logger.info(f"Started reading data from {path}")
     data = pd.read_csv(path)
+    logger.info(f"Finished reading data")
     return data
