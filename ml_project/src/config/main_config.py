390b048f8f36382fcc56a9145e4edd25ddef5414
voiteshonok <shnobdog1@gmail.com>
2021-04-20 15:28:13 +0300

    Add experiment tracking config

--- ml_project/src/config/main_config.py
+++ ml_project/src/config/main_config.py
@@ -3,8 +3,20 @@ from omegaconf import MISSING
 
 
 @dataclass
+class TrackConfig:
+    track_experiment: bool = MISSING
+    experiment_data_dir: str = MISSING
+    save_model_weights: bool = MISSING
+    metrics_file_name: str = MISSING
+    config_file_name: str = MISSING
+    model_weights_file_name: str = MISSING
+    experiment_dir_name_format: str = MISSING
+
+
+@dataclass
 class MainConfig:
     input_data_path: str = MISSING
     eda_dir: str = MISSING
     random_seed: int = MISSING
     target_name: str = MISSING
+    track: TrackConfig = MISSING
